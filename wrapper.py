353dbb3118687c26e36d5ef392bde0597eb1fef7
marcellobeltrami <marcello.beltrami@gmail.com>
2024-01-30 22:58:49 +0000

    initial commit

---
+++ wrapper.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+#Import libraries
+import argparse
+import os 
+import sys
+
+#Creates parser object. Arguments can then be added to object parser. 
+parser_1 = argparse.ArgumentParser(description="")
+
+
+
