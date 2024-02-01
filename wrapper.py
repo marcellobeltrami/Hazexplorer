974f344ef3ba09b9dd96f9e94b13fcac54fa1f14
marcellobeltrami <marcello.beltrami@gmail.com>
2024-02-01 10:10:28 +0000

    wrapper comments

--- wrapper.py
+++ wrapper.py
@@ -5,6 +5,10 @@ import argparse
 import os 
 import sys
 
+
+#Ensure that for steps where SLURM is used, add something that checks whether a job has been completed/failed
+#Raise a flag if it fails.  
+
 #Creates parser object. Arguments can then be added to object parser. 
 parser_1 = argparse.ArgumentParser(description="")
 
