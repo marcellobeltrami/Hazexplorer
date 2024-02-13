f3bad79cd50b15880e8dea606b7bc29eaa89f29c
marcellobeltrami <marcello.beltrami@gmail.com>
2024-02-13 23:27:04 +0000

    added nextflow script generation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import argparse
 import os 
 import sys
 import yaml
-
+import main_modules
 #This pipeline will run on a single set of reads. It will then be parallelized on SLURM by user.  
 
 #Creates parser and subparsers objects.  
@@ -13,35 +13,36 @@ import yaml
 main_parser= argparse.ArgumentParser(prog="Hazexplorer", 
                                  description="Pipeline allowing SNPs calling from bisulphite sequencing data.")
 
-subparser = main_parser.add_subparsers(title='subcommands', dest='subcommand', help='quality_check, align, SNP_call')
+nextflow_script = open("Hazexplorer.nt", "w")
+
+subparser = main_parser.add_subparsers( dest='subcommand', help='quality_check, align, SNP_call')
 
 #-----------------------------------------------------#
 # Creates and defines parser for quality and trimming #
 #-----------------------------------------------------#
 parser_quality= subparser.add_parser("quality_check", help="Carries out quality check and trimming")
 
-#Adds arguments available, parses arguments inputted, assigns relative arguments to variable
+#Adds arguments available, p
+parser_quality.add_argument("-1", "--single_end", help="using this flag allows to use single paired reads. Provide ")
 parser_quality.add_argument("-t", "--trim", default=True ,help="carries out trimming of the fastq files to remove adapters and other trimming using trimmomatic.")
-parser_quality.add_argument("-p", "--paired_ends", default=False ,help="setting this to True,  tells trimming to expect as paired ends")
-parser_quality.add_argument("-q", "--quality_control", default=True ,help="carries out quality control using FastQC. Set to False to disable it")
-parser_quality.add_argument("-s", "--slurm_scheduler", default=False, help="set use of slurm (sbatch) to true or false. job will be submitted if slurm is set True.")
-
+parser_quality.add_argument("-2", "--paired_ends", default=False ,help="adding a second read activates pair end mode, tells trimming to expect paired ends")
+parser_quality.add_argument("-o", "--output", default="./results/QC/", help="change location of output directory")
+#arses arguments inputted, assigns relative arguments to variable
 args_qual = parser_quality.parse_args()
+single_en = args_qual.single_end
+pair_en = args_qual.paired_ends
+trim_option= args_qual.trim
+output_QC = args_qual.output
+#Here insert logic and "backend" for trimming.
 
-pair_en = args_qual.paired_ends()
-trim_option= args_qual.trim()
-QC = args_qual.quality_control()
-slurm = args_qual.slurm_scheduler()
-#Here insert logic and "backend" of your app.
+if args_qual.subcommand == "quality_check":
+
+    main_modules.trim_qc_nt(single_en, output_QC,nextflow_script)
+    if pair_en != False: 
+        main_modules.trim_qc_nt(single_en, pair_en, output_QC,nextflow_script)  
+
 
 #Ask joe if sbatch should be included within pipeline or the pipeline will be run with sbatch itself.
-
-
-#PSEUDOCODE: 
-#  - set up a yaml file first. 
-#  - change parameters in yaml file base on the one inputted by user. 
-#  - run the reads_quality.sh script by using input parameters from yaml file. 
-#  - 
 
 #--------------------------------------------------#
 # Creates and defines parser for genome alignment  #
@@ -59,3 +60,4 @@ parser_SNP= subparser.add_parser("SNP_call", help="Carries out SNP calling")
 
 
 
+##Add a section that parses the nextflow script checking for specific process and composes the workflow section. 
